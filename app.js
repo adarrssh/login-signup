5281d36764f02c84dfd86eabb8ef93e5d62d2997
adarrssh <adarsh00502@gmail.com>
2022-05-11 22:41:24 +0530

    add

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { default: mongoose, mongo } = require('mongoose');
+const app = express();
+require("./models/user")
+const port= process.env.PORT||5000
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+mongoose.connect("mongodb+srv://adarsh:adarsh@cluster0.lnxnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("Connected to mongodb");
+})
+
+mongoose.connection.on("error",()=>{
+    console.log("Error !!");
+})
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(port,()=>{
+    console.log("server running");
+})
---
+++ mongourl.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURL:"mongodb+srv://adarsh:adarsh@cluster0.lnxnv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
